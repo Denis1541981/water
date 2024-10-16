69bac60c52eaa3acef563fcdaf0f4c447ebeb3c4
den <turin8908@gmail.com>
2024-10-16 19:55:04 +0700

    Start

---
+++ api_pogoda.py
@@ -0,0 +1,89 @@
+#! /usr/bin/env python
+import math
+from asyncio import timeout
+
+import requests
+import pytz
+import datetime
+from aiogram.utils.markdown import hbold
+from geolocation import geolocate
+
+class WeatherAPI:
+    def __init__(self, api_key: str, text: str):
+        self.api_key = api_key
+        self.text = text
+
+    def get_weather_data(self, text:str) -> dict:
+        lat, lon = geolocate(self.text)
+        url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&lang=ru&units=metric&appid={self.api_key}"
+        res = requests.get(url)
+        if res.status_code == 200:
+            return res.json()
+        return {}
+
+class WeatherFormatter:
+    @staticmethod
+    def format_weather_message(data: dict, user_name: str) -> str:
+        if data:
+            city = data.get("name")
+            cur_temp = round(data.get("main", {}).get("temp"))
+            humidity = data.get("main", {}).get("humidity")
+            pressure = data.get("main", {}).get("pressure")
+            wind = data.get("wind").get("speed")
+            feels_like = round(data["main"]["feels_like"])
+            sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"].get("sunrise"))
+            sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"].get("sunset"))
+            length_of_the_day = sunset_timestamp - sunrise_timestamp
+            weather_description = data["weather"][0].get("description")
+            wd = WeatherFormatter.get_smile(weather_description)
+
+            message = (
+                f"Замер произведен в {WeatherFormatter.get_current_time()}\n"
+                f"Привет, {hbold(user_name)}!\n"
+                f"Погода в городе {hbold(city)}:\n"
+                f"Температура: {hbold(cur_temp)}°C {wd}\n"
+                f"Ощущается как: {hbold(feels_like)}°C\n"
+                f"Влажность: {hbold(humidity)}%\n"
+                f"Давление: {hbold(math.ceil(pressure / 1.333))} мм.рт.ст\n"
+                f"Ветер: {hbold(wind)} м/с\n"
+                f"Восход солнца: {sunrise_timestamp}\n"
+                f"Закат солнца: {sunset_timestamp}\n"
+                f"Продолжительность дня: {hbold(length_of_the_day)}\n"
+                f"{hbold(WeatherFormatter.get_time_of_the_day())}  {hbold(user_name)}!"
+            )
+
+            return message
+        else:
+            return "Не удалось получить данные о погоде."
+
+    @staticmethod
+    def get_smile(weather_description: str) -> str:
+        weather_smile = {
+            "ясно": "Ясно \U00002600",
+            "облачно": "Облачно \U00002601",
+            "дождь": "Дождь \U00002614",
+            "гроза": "Гроза \U000026A1",
+            "снег": "Снег \U0001F328",
+            "туман": "Туман \U0001F32B",
+            "пасмурно": "Пасмурно \U0001F32B",
+        }
+        return weather_smile.get(weather_description.lower(), "\nПосмотри в окно, я не понимаю, что там за погода...")
+
+    @staticmethod
+    def get_current_time() -> str:
+        mess_date = datetime.datetime.now()
+        tz = pytz.timezone("Etc/GMT-7")
+        mess_date_utc5 = tz.normalize(mess_date.astimezone(tz))
+        return mess_date_utc5.strftime("%H:%M:%S")
+
+    @staticmethod
+    def get_time_of_the_day() -> str:
+        current_time = datetime.datetime.strptime(WeatherFormatter.get_current_time(), "%H:%M:%S")
+        if 5 <= current_time.hour <= 9:
+            return 'Доброго утра!'
+        elif 10 <= current_time.hour <= 16:
+            return 'Хорошего дня!'
+        elif 17 <= current_time.hour <= 20:
+            return 'Добрый вечер!'
+        else:
+            return 'Доброй ночи!'
---
+++ pogoda_bot.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+#!coding:utf-8
+import asyncio
+import logging
+import sys
+from datetime import datetime
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.bot import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from api_pogoda import WeatherFormatter, WeatherAPI
+from config import TOKEN, API
+from geolocation import logger
+
+# Логирование
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+file_handler = logging.FileHandler('log.log', encoding='utf-8', mode='w')
+logger.addHandler(handler)
+logger.addHandler(file_handler)
+
+dp = Dispatcher()
+
+
+# Хэндлер для команды /start
+@dp.message(CommandStart())
+async def start_command(message: Message):
+    try:
+        await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды")
+    except Exception as e:
+        logger.error(f"start command: {e}")
+
+
+# Хэндлер для получения погоды
+@dp.message()
+async def get_weather(message: Message):
+    try:
+        await message.answer(f"Смотрю погоду в городе {message.text}")
+        weather_api = WeatherAPI(API, message.text)
+        weather_data = weather_api.get_weather_data(message.text)
+        messag = WeatherFormatter.format_weather_message(weather_data, message.from_user.first_name)
+        await message.answer(messag)
+    except Exception as e:
+        logger.error(f"get_water: {e}")
+        await message.answer("Что-то пошло не так! Проверяю!!!")
+
+
+
+async def main():
+    # # Настройка прокси
+    # proxy_url = f'http://{login_proxy}:{parole_proxy}@217.29.53.105:12529'
+    # session = AiohttpSession(proxy=proxy_url)
+    logger.info(f"Start: {datetime.now()}")
+    # Инициализация бота
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    try:
+        # Удаление вебхуков и запуск поллинга
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot, polling_timeout=60)
+        await bot.close(request_timeout=30)
+    except Exception as e:
+        logger.error(f"Error: {e}")
+    except KeyboardInterrupt:
+        logger.info(f"Завершение программы {datetime.now()}")
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info("Завершение программы")
